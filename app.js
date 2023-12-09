a6c692c97f318c0182322c00bde709c51b52b800
ULTRAPAIN <1032210157@tcetmumbai.in>
2023-12-09 11:30:00 +0530

    server updated

--- app.js
+++ app.js
@@ -50,6 +50,6 @@ app.post('/', function(req, res){
         res.redirect('/');
     }
 });
-app.listen(3000,function(){
-    console.log('listening on port 3000');
-}); 
+app.listen(process.env.PORT||3000,function(){
+    console.log("Server Started listening on port 3000")
+})
